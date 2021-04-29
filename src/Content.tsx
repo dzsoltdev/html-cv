bc34e8b26287f9301f43d951e98856d68a671b06
Zsolt Dobak <zsolt.dobak@oracle.com>
2021-04-29 08:56:12 +0200

    Improve project details

--- src/Content.tsx
+++ src/Content.tsx
@@ -172,7 +172,7 @@ const Content = () => {
 
                   <section>
                     <div className={'sub-title'}>ProMed Bt's webpage</div>
-                    <a className={'info'} href={'https://promedbt.hu/'}>https://promedbt.hu</a>
+                    <a className={'info'} href={'https://promedbt.hu/'} target={'_blank'} rel="noreferrer">https://promedbt.hu</a>
                     <div className={'label'}>Tech. Stack</div>
                     <div>HTML, SCSS, JavaScript, React, npm, GitHub, Firebase</div>
                     <div className={'label'}>Project Summary</div>
@@ -181,9 +181,27 @@ const Content = () => {
                         I've made this small web app, written in ReactJS with a simple design to the ProMed Bt.
                         It was a special project for me, because as a freelancer, I was the only responsible for the entire
                         project, including the communication
-                        with custumer about the needs and also about the opinions.
+                        with customer about the needs and also about the opinions.
                         I extended the design and the features continuously, following the agile methodology and also made
                         the deployment to Firebase.
+                      </p>
+                    </div>
+                  </section>
+
+                  <section data-breakpoint={true}>
+                    <div className={'sub-title'}>EvoSol 2020 Ltd's webpage</div>
+                    <a className={'info'} href={'https://evosol.hu/'} target={'_blank'} rel="noreferrer">https://evosol.hu</a>
+                    <div className={'label'}>Tech. Stack</div>
+                    <div>HTML, SCSS, TypeScript, React, npm, GitHub, Firebase, Cloudinary, Google Maps</div>
+                    <div className={'label'}>Project Summary</div>
+                    <div>
+                      <p>
+                        It was my second freelancer project, with much more feature like the previous one.
+                        It is also written in React with typescript, but focuses on attracting customers to improve publicity.
+                        For the best user experience, I implemented animated loaders for images, used
+                        <a href={'https://cloudinary.com/'} target={'_blank'} rel="noreferrer"> Cloudinary</a> as a gallery components, which
+                        uses CDN to improve customer setiscafction. I also integrated Google Maps and finally launched in Firebase as well as the
+                        previous project.
                       </p>
                     </div>
                   </section>
@@ -205,7 +223,7 @@ const Content = () => {
                     <div className={'summary'}>
                       <div className={'sub-title'}>Principal UI and mobile developer at Oracle</div>
                       <div>November, 2019 - Present</div>
-                      <a className={'info'} href={'https://www.oracle.com/'}>https://www.oracle.com</a>
+                      <a className={'info'} href={'https://www.oracle.com/'} target={'_blank'} rel="noreferrer">https://www.oracle.com</a>
                     </div>
                   </Tooltip>
                 </AccordionSummary>
@@ -265,7 +283,7 @@ const Content = () => {
                     <div className={'summary'}>
                       <div className={'sub-title'}>Senior Software Developer at IncepTech</div>
                       <div>March, 2017 - November, 2019</div>
-                      <a className={'info'} href={'https://www.incepteam.com/'}>https://www.incepteam.com</a>
+                      <a className={'info'} href={'https://www.incepteam.com/'} target={'_blank'} rel="noreferrer">https://www.incepteam.com</a>
                     </div>
                   </Tooltip>
                 </AccordionSummary>
@@ -279,13 +297,10 @@ const Content = () => {
                     <div className={'label'}>Project Summary</div>
                     <div>
                       <p>
-                        This tool is the responsible for the schedule optimization in the most of the Bayer's factories in
-                        Italy and Germany.
-                        At the beginning, it's written in ReactJS by another team, later me and my team got the project to
-                        investigate and fix existing issues
-                        and implement new features on it. My additional role was to help new colleagues to pick up the
-                        necessary knowledge in front end development,
-                        mainly in ReactJS.
+                        This tool is the responsible for the schedule optimization in the most of the
+                        <a href={'https://www.bayer.com'} target={'_blank'} rel="noreferrer"> Bayer</a>'s factories in all around the world.
+                        At the beginning, it's written in ReactJS by another team, later me and my team got the project to investigate and fix existing issues
+                        and implement new features on it. My additional role was to help new colleagues to pick up the necessary knowledge in front end development.
                       </p>
                     </div>
                   </section>
@@ -293,33 +308,34 @@ const Content = () => {
                   <section data-breakpoint={true}>
                     <div className={'sub-title'}>Sequent</div>
                     <div className={'label'}>Tech. Stack</div>
-                    <div>HTML, SCSS, JavaScript, React, Redux, webpack, REST API, npm, git</div>
+                    <div>HTML, SCSS, JavaScript, React, Redux, webpack, REST API, npm, git, AWS, CDN</div>
                     <div className={'label'}>Project Summary</div>
                     <div>
                       <p>
-                        It was the first in-house project in the history of IncepTech Ltd, where I was responsible for
-                        building the core ReactJS modules
-                        of an innovative visual event planning platform. Due to its nature as an in-house startup, I was
-                        given free reign in trying out
-                        new React methodologies to optimize our feature development pipeline, like building reusable app
-                        components.
+                        It was the first in-house project in the history of IncepTech Ltd, where I was responsible for building the core ReactJS modules
+                        from scratch of an innovative visual event planning platform. Due to its nature as an in-house startup, I was given free reign in trying out
+                        new React practices to optimize our feature development and building reusable app components. The main feature was to visualize an event
+                        as a directed graph, which has been implemented with using D3 and GSAP, embedded into React wrapper components.
                       </p>
                     </div>
                   </section>
 
                   <section data-breakpoint={true}>
                     <div className={'sub-title'}>Parametric CleanSheet</div>
+                    <a className={'info'} href={'https://www.mckinsey.com/business-functions/operations/how-we-help-clients/cleansheet/parametric-modeling'} target={'_blank'} rel="noreferrer">
+                      Link to the original project overview
+                    </a>
                     <div className={'label'}>Tech. Stack</div>
                     <div>HTML, SCSS, JavaScript, React, Redux, webpack, REST API, npm, git, Auth0, nodeJS</div>
                     <div className={'label'}>Project Summary</div>
                     <div>
                       <p>
-                        It is a cost engineering tool (owned by McKinsey & Company), built to optimize manufacturing
-                        processes of large multinationals, based on advanced benchmarking analytics.
-                        I was part of the team responsible for the entire redesign & implementation of the UI, based on
-                        the
-                        ReactJS framework,
-                        as well as implementing test-driven methodologies within the Scrum feature team.
+                        It is a cost engineering tool (owned by <a href={'https://www.mckinsey.com'} target={'_blank'} rel="noreferrer"> McKinsey & Company</a>),
+                        built to optimize manufacturing processes of large multinationals, based on advanced benchmarking analytics.
+                        I was part of the team, responsible for the entire redesign & implementation of the UI with ReactJS.
+                        The main goal of the refactor was to efficiently handle huge datasets, so I had te opportunity to collect valuable
+                        experience about windowed list with infinite scroll, continuously extendable forms, which can have embedded forms as well,
+                        dynamic charts, etc.
                       </p>
 
                       <p>
@@ -332,17 +348,18 @@ const Content = () => {
 
                   <section data-breakpoint={true}>
                     <div className={'sub-title'}>Performance Lens</div>
+                    <a className={'info'} href={'https://www.mckinsey.com/industries/financial-services/how-we-help-clients/performance-lens/overview'} target={'_blank'} rel="noreferrer">
+                      Link to the original project overview
+                    </a>
                     <div className={'label'}>Tech. Stack</div>
                     <div>HTML, SCSS, JavaScript, AngularJs, Redux, gulp, REST API, npm, git</div>
                     <div className={'label'}>Project Summary</div>
                     <div>
                       <p>
-                        This is a document manager tool (owned by McKinsey & Company) to help organize periodically
-                        generated surveys and client responses
-                        with custom templates for McKinsey & Company and partner companies. It was a small project with a
-                        short, one month development period,
-                        in which my team’s role was to plan and deliver a web based solution with the given
-                        functionalities.
+                        This is a document manager tool (owned by <a href={'https://www.mckinsey.com'} target={'_blank'} rel="noreferrer"> McKinsey & Company</a>)
+                        to help organize periodically generated surveys and client responses with custom templates for McKinsey & Company and partner companies.
+                        It was a small project with a short, one month development period, in which my team’s role was to plan and deliver a
+                        web based solution with the given functionalities.
                       </p>
                     </div>
                   </section>
@@ -350,21 +367,14 @@ const Content = () => {
                   <section data-breakpoint={true}>
                     <div className={'sub-title'}>SectorTech</div>
                     <div className={'label'}>Tech. Stack</div>
-                    <div>HTML, SCSS, JavaScript, AngularJs, Redux, gulp, REST API, npm, git</div>
+                    <div>HTML, SCSS, JavaScript, AngularJs, Redux, gulp, webpack, REST API, npm, git</div>
                     <div className={'label'}>Project Summary</div>
                     <div>
                       <p>
-                        This project is a remade and extended version of FinTech (see below) to explore various sectors in
-                        a
-                        structured way and
-                        get insights straight from the leading startups and experts of the field. Our role was also to
-                        plan
-                        and estimate
-                        the sprint task based on the given user stories. I was one of the three front end developers on
-                        the
-                        project and received special
-                        tasks like creating the build system of the front end part to deploy it to different environments,
-                        and to keep the used 3rd party
+                        This project is a remade and extended version of FinTech (see below) to explore various sectors in a structured way and
+                        get insights straight from the leading startups and experts of the field. Our role was also to plan and estimate
+                        the sprint task based on the given user stories. I was one of the three front end developers on the project and received special
+                        tasks like creating the build system of the frontend part to deploy it to different environments and to keep the used 3rd party
                         components up to date.
                       </p>
                     </div>
@@ -372,17 +382,19 @@ const Content = () => {
 
                   <section data-breakpoint={true}>
                     <div className={'sub-title'}>FinTech</div>
+                    <a className={'info'} href={'https://www.mckinsey.com/industries/financial-services/how-we-help-clients/panorama/our-offerings/panorama-fintech'} target={'_blank'} rel="noreferrer">
+                      Link to the original project overview
+                    </a>
                     <div className={'label'}>Tech. Stack</div>
                     <div>HTML, SCSS, JavaScript, AngularJs, Redux, gulp, REST API, npm, git</div>
                     <div className={'label'}>Project Summary</div>
                     <div>
                       <p>
-                        This tool is a knowledge platform for banks and other financial institutions (owned by McKinsey &
-                        Company) to help them stay ahead
-                        of digital disruption. As a beginner AngularJS developer, I joined the team on the maintaining
-                        stage
-                        of the project,
-                        but soon I got my first own tasks to enhance the solution with new features.
+                        This tool is a knowledge platform for banks and other financial institutions (owned by
+                        <a href={'https://www.mckinsey.com'} target={'_blank'} rel="noreferrer"> McKinsey & Company</a>) to help them stay ahead
+                        of digital disruption. I joined to the team as a beginner AngularJs developer on the maintaining
+                        stage of the project. My performance to adapt to AngularJs highlighted my strength to the team
+                        and soon I got my first own tasks to enhance the solution with new features.
                       </p>
                     </div>
                   </section>
@@ -404,7 +416,7 @@ const Content = () => {
                     <div className={'summary'}>
                       <div className={'sub-title'}>Software Engineer at Nokia</div>
                       <div>Augustus, 2014 - March, 2017</div>
-                      <a className={'info'} href={'https://www.nokia.com/'}>https://www.nokia.com</a>
+                      <a className={'info'} href={'https://www.nokia.com/'} target={'_blank'} rel="noreferrer">https://www.nokia.com</a>
                     </div>
                   </Tooltip>
                 </AccordionSummary>
@@ -414,14 +426,16 @@ const Content = () => {
                   <section>
                     <div className={'sub-title'}>nWidgets</div>
                     <div className={'label'}>Tech. Stack</div>
-                    <div>HTML, SCSS, JavaScript, React, Flux, gulp, REST API, Karma, Mocha, Robot, npm, git, Jenkins</div>
+                    <div>HTML, SCSS, JavaScript, React, Flux, gulp, REST API, Karma, Mocha, Robot, npm, git, Jenkins, Agile</div>
                     <div className={'label'}>Project Summary</div>
                     <div>
                       <p>
                         It was my first project as a front end developer in collaboration with colleagues from abroad on a
-                        system analytical tool for mobile networks,
-                        I created & extended ReactJS components with keeping TDD and A-TDD rules, while also maintained the
-                        existing code base and test case set.
+                        system analytical tool for mobile networks. This tool was responsible for collecting and display available data
+                        about the operating network system by customers. Eg. it was able to visualize the network usage on a given area or
+                        a given transmission tower on a map as well as on charts or tables. MY team followed the agile methodologies and my role
+                        was to created and extended ReactJS components with keeping TDD and A-TDD rules, while also to maintain the
+                        existing code base and test case set. During this project I participated in an internal scrum master training.
                       </p>
                     </div>
                   </section>
@@ -434,9 +448,10 @@ const Content = () => {
                     <div>
                       <p>
                         I think, it isn't necessary to introduce 3G mobile systems, which was my first project in corporate
-                        environment.
-                        I made performed development and maintenance works on the 3G core modules of Nokia system, also
-                        prepared module tests.
+                        environment. As a junior developer, my main task was to improve my knowledge about programming methodologies
+                        and the 3GPP standard. I also were responsible to made performed development and maintenance works on
+                        the 3G core modules of Nokia system (usually on A-interface), also prepared module tests. I also had to take
+                        courses, such as Clean code (by Robert Martin), scrum and agile.
                       </p>
                     </div>
                   </section>
